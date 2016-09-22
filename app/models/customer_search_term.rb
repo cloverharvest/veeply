6f0f48a9e46e38f7af726afce76898dce3f7291c
cloverharvest <anna@schwabdev.com>
2016-09-21 22:38:11 -0700

    Makes index method for simple search and corrects code spacing error in customer search term model to complete simple search feature using Postgres

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -2,9 +2,9 @@ class CustomerSearchTerm < ApplicationRecord
   attr_reader :where_clause, :where_args, :order
   def initialize(search_term)
     search_term=search_term.downcase
-    @where_clause=""
-    @where_args={}
-    if search_term=~/@/
+    @where_clause = ""
+    @where_args = {}
+    if search_term =~ /@/
       build_for_email_search(search_term)
     else
       build_for_name_search(search_term)
@@ -12,33 +12,38 @@ class CustomerSearchTerm < ApplicationRecord
   end
 
 private
-  def build_for_email_search(search_term)
-    @where_clause<<case_insensitive_search(:first_name)
-    @where_args[:first_name]=starts_with(extract_name(search_term))
 
-    @where_clause<< " OR#{case_insensitive_search(:'last_name')}"
+  def build_for_email_search(search_term)
+    @where_clause << case_insensitive_search(:first_name)
+    @where_args[:first_name] = starts_with(extract_name(search_term))
+
+    @where_clause << " OR #{case_insensitive_search(:'last_name')}"
     @where_args[:last_name]=starts_with(extract_name(search_term))
 
-    @where_clause<< " OR#{case_insensitive_search(:email)}"
-    @where_args[:email]=search_term
+    @where_clause<< " OR #{case_insensitive_search(:email)}"
+    @where_args[:email] = search_term
 
-    @order="lower(email) = "+
-      ActiveRecord::Base.connection.quote(search_term)+
+    @order = "lower(email) = " +
+      ActiveRecord::Base.connection.quote(search_term) +
       "desc, last_name asc"
   end
-  
+
+  def extract_name(email)
+    email.gsub(/@.*$/,'').gsub(/[0-9]+/,'')
+  end
+
   def build_for_name_search(search_term)
-    @where_clause<<case_insensitive_search(:first_name)
+    @where_clause << case_insensitive_search(:first_name)
     @where_args[:first_name] = starts_with(search_term)
 
-    @where_clause<< " OR#{case_insensitive_search(:last_name)}"
-    @where_args[:'last_name']=starts_with(search_term)
+    @where_clause<< " OR #{case_insensitive_search(:last_name)}"
+    @where_args[:'last_name'] = starts_with(search_term)
 
     @order="last_name asc"
   end
 
   def starts_with(search_term)
-    search_term+ "%"
+    search_term + "%"
   end
 
   def case_insensitive_search(field_name)
