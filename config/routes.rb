94b9c7525a08d05e5d432a51c2a87e6f1d23903f
cloverharvest <anna@schwabdev.com>
2016-09-22 22:23:04 -0700

    Builds simple angular app to test install

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'customers/index'
 
   devise_for :users
-
   root 'dashboard#index'
   resources :customers, only:[ :index ]
+
+  get "angular_test", to: "angular_test#index"
 end
