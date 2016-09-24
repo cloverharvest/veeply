a66e9e144ce8665ab0159cc80a809c17c570953d
cloverharvest <anna@schwabdev.com>
2016-09-23 23:16:47 -0700

    Converts remaining erb to angular, tests results view with canne d results

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -4,8 +4,32 @@ var app = angular.module('customers', [ ]);
 app.controller("CustomerSearchController", [
         "$scope",
   function($scope) {
+    $scope.customers = [];
     $scope.search = function(searchTerm) {
-      $scope.searchedFor = searchTerm;
+      $scope.customers = [
+        {
+          "first_name":"Schwab",
+          "last_name":"Anna",
+          "email":"anna@schwab.net",
+          "username":"annas",
+          "created_at":"2016-09-23",
+        },
+        {
+          "first_name":"Sony",
+          "last_name":"Seiko",
+          "email":"sony@seiko.com",
+          "username":"sonys",
+          "created_at":"2016-09-23",
+        },
+        {
+          "first_name":"Toshiba",
+          "last_name":"Holmes",
+          "email":"toshiba@homes.com",
+          "username":"toshibah",
+          "created_at":"2016-09-23",
+        }
+      ]
+      
     }
   }
 ]);
