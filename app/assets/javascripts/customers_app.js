7f9b8f32828b673b9681db9b993e93ef0bf75b01
cloverharvest <anna@schwabdev.com>
2016-09-24 20:17:43 -0700

    Changed to ajax request, convert timestamp with date filter and update readme

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -2,34 +2,18 @@
 var app = angular.module('customers', [ ]);
 
 app.controller("CustomerSearchController", [
-        "$scope",
-  function($scope) {
+        "$scope","$http",
+  function($scope, $http) {
     $scope.customers = [];
     $scope.search = function(searchTerm) {
-      $scope.customers = [
-        {
-          "first_name":"Schwab",
-          "last_name":"Anna",
-          "email":"anna@schwab.net",
-          "username":"annas",
-          "created_at":"2016-09-23",
-        },
-        {
-          "first_name":"Sony",
-          "last_name":"Seiko",
-          "email":"sony@seiko.com",
-          "username":"sonys",
-          "created_at":"2016-09-23",
-        },
-        {
-          "first_name":"Toshiba",
-          "last_name":"Holmes",
-          "email":"toshiba@homes.com",
-          "username":"toshibah",
-          "created_at":"2016-09-23",
-        }
-      ]
-      
-    }
+      $http.get("/customers.json",
+            { "params": { "keywords":searchTerm } }
+      ).then(function(response) {
+        $scope.customers = response.data;
+      }, function(response) {
+        alert("There was a problem: " + response.status);
+      }
+    );
+   }
   }
 ]);
