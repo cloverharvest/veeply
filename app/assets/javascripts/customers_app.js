bc8b90e8852560ff22933ddb8234d2d0eea56618
cloverharvest <anna@schwabdev.com>
2016-10-10 21:54:37 -0700

    Adds unit test of angular code

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -11,27 +11,28 @@ app.controller("CustomerSearchController", [
       if( searchTerm.length < 3) {
         return;
       }
-      
+
       $http.get("/customers.json",
             { "params": { "keywords":searchTerm, "page":page } }
       ).success(
         function(data,status,headers,config) {
         $scope.customers = data;
+        $scope.loading = false;
       }).error(
         function(data,status,headers,config) {
         alert("There was a problem: " + status);
       });
-   }
+   };
 
    $scope.previousPage = function() {
      if (page > 0) {
        page = page - 1;
        $scope.search($scope.keywords);
      }
-   }
+   };
    $scope.nextPage = function() {
      page = page + 1;
      $scope.search($scope.keywords);
-   }
+   };
   }
 ]);
---
+++ spec/javascripts/controllers/customer_search_controller_spec.js
@@ -0,0 +1,22 @@
+
+
+describe("CustomerSearchController", function(){
+  describe("Initialization",function(){
+
+    var scope = null,
+        controller = null;
+
+    beforeEach(module("customers"));
+
+    beforeEach(inject(function ($controller,$rootScope) {
+      scope      = $rootScope.new();
+      controller = $controller("CustomerSearchController",{
+       $scope: scope
+     });
+   }));
+ //tests here
+    it("defaults to an empty customer list", function(){
+      expect(scope.customers).toEqualData([]);
+    });
+ });
+});
