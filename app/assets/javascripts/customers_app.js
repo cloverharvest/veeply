ece985c27300b02f00617814f3de299f197af9aa
cloverharvest <anna@schwabdev.com>
2016-10-12 10:05:26 -0700

    Second try:configure function to get  passed intoit

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,5 +1,18 @@
 // This will be picked up by the asset pipeline
-var app = angular.module('customers', ['ngRoute']);
+var app = angular.module('customers', ['ngRoute', 'templates']);
+
+app.config([
+          "$routeProvider",
+  function($routeProvider) {
+
+    // configure our routes here...
+
+    $routeProvider.when("/", {
+      controller: "CustomerSearchController",
+      templateUrl: "customer_search.html"
+    });
+  }
+]);
 
 app.controller("CustomerSearchController", [
         "$scope","$http",
